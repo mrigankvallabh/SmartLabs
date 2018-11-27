51dcaecd24df5cd0e7b59f016fb40d2198801812
exit <mrigankvallabh@gmail.com>
2018-11-27 14:41:23 +0800

    Added Sample Chart

---
+++ js/slab-chart.js
@@ -0,0 +1,62 @@
+
+$.getJSON(
+  'https://mrigankvallabh.github.io/SmartLabs/sam.json',
+  function (data) {
+
+    Highcharts.chart('myHighChart', {
+      chart: {
+        zoomType: 'x'
+      },
+      title: {
+        text: 'BTS Point 1 Chart Dated 6-Oct-2018'
+      },
+      subtitle: {
+        text: document.ontouchstart === undefined ?
+            'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      yAxis: {
+        title: {
+          text: 'Velocity (rms)'
+        }
+      },
+      legend: {
+        enabled: true
+      },
+      plotOptions: {
+        area: {
+          fillColor: {
+            linearGradient: {
+              x1: 0,
+              y1: 0,
+              x2: 0,
+              y2: 1
+            },
+            stops: [
+              [0, Highcharts.getOptions().colors[0]],
+              [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+            ]
+          },
+          marker: {
+            radius: 2
+          },
+          lineWidth: 1,
+          states: {
+            hover: {
+              lineWidth: 1
+            }
+          },
+          threshold: null
+        }
+      },
+
+      series: [{
+        type: 'line',
+        name: 'BTS Point 1',
+        data: data
+      }]
+    });
+  }
+);
